be0eb3befa3fd265c5747d4dddefcf0f783db5ff
taegan <warretaega@gmail.com>
2020-11-10 20:09:18 -0800

    Basic bot functionality finished

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # discord-honeypot-bot
+
+Simple honeypot discord bot written with discord.js 11.6.4 and node.js
+
+Waits for direct messages and upon recieving one, starts collecting all additional messages in the direct message channel for 10 seconds, resetting everytime an additional message is recieved.
+
+After the timer runs out, creates an embed with the username of the user who sent the direct message(s) and all the message(s) they sent. Then sends the embed to the text channel provided by the `CHANNEL` variable in the `.env` file.
+
+This bot, as is, is meant to be self hosted and run on only a single server, with preferably a staff channel given as the channel to send embed to.
+
+This bot will not be updated to run on discord.js versions higher than 11.6.4 as this version of discord.js allows the bot to login to a normal user account instead of a bot account.
+
+---
+
+**.env file variables:**
+
+`TOKEN`: The account token to login with. Either a user account token or bot account token.
+
+`GUILD`: The guild ID of the server the bot should run in.
+
+`CHANNEL`: The channel ID of the channel the bot should send embeds to. Must be a channel in the provided guild.
---
+++ bot.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+
+const { Client, MessageCollector, RichEmbed } = require('discord.js');
+const bot = new Client({ disableEveryone: true });
+
+let active_collectors = [];
+let guild;
+
+bot.on('ready', async () => {
+    console.log(`Logged in as: ${bot.user.tag}!`);
+    guild = await bot.guilds.get(process.env.GUILD);
+});
+
+bot.on('message', async (message) => {
+    if (message.author.bot) return;
+    const filter = (m) => m.author.id === message.author.id;
+    if (message.channel.type === 'dm') {
+        if (active_collectors.includes(message.channel.id)) {
+            return;
+        } else {
+            active_collectors.push(message.channel.id);
+            const collector = new MessageCollector(message.channel, filter, {
+                idle: 10000
+            });
+            collector.collected.set(message.id, message);
+            collector.on('end', (collected) => {
+                active_collectors.splice(active_collectors.indexOf(message.channel.id), 1);
+                const notify_channel = guild.channels.get(process.env.CHANNEL);
+                const messages = collected.array();
+                let dm_embed = new RichEmbed()
+                    .setTitle(message.author.username)
+                    .setThumbnail(message.author.avatarURL)
+                    .addField('User ID', message.author.id);
+                let messages_string = '';
+                messages.forEach( (message) => {
+                    messages_string = messages_string + message.content + '\n';
+                });
+                dm_embed.addField('Message(s)', messages_string);
+                notify_channel.send(dm_embed);
+            });
+        }
+    }
+});
+
+bot.login(process.env.TOKEN);
